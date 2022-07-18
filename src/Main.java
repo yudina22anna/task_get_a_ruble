e3470a4df31af29d36058a3c2fcd731458eeea6b
annayudina <yudina.anna.zi15@gmail.com>
2022-07-18 17:42:44 +0300

    test

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 100;
-        int refill = 1100;
+        int refill = 1110;
         int bonus;
-        if (refill<1000){
+        if (refill < 1000) {
             bonus = 0;
-        }
-        else {
-            bonus = refill/100;
+        } else {
+            bonus = refill / 100;
         }
         int totalBalance = balance + refill + bonus;
-        System.out.println(total_balance);
+        System.out.println(totalBalance);
     }
 }

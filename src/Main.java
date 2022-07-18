2b27690b8eae3e561b46123aec238267d9855c32
annayudina <yudina.anna.zi15@gmail.com>
2022-07-18 20:00:50 +0300

    refill 1100

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 100;
-        int refill = 1110;
+        int refill = 1100;
         int bonus;
         if (refill < 1000) {
             bonus = 0;
